0cce11be6026a741352d01be8a16745937cbd595
LeonardoMinniti <33843867+LeonardoMinniti@users.noreply.github.com>
2018-08-09 17:51:39 +0200

    Server and tic tac toe

---
+++ Assets/Scripts/Client/Client.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using System.IO;
+using System.Net.Sockets;
+using UnityEngine;
+using System;
+
+public class Client : MonoBehaviour {
+
+    public GameObject ChatContainer;
+    public GameObject MessagePrefab;
+    public GameObject HostButton;
+    public GameObject LoginPanel;
+    public GameObject TicTacToe;
+
+
+    public string clientName;
+
+    private bool socketReady;
+    private string tempSendInfo;
+    private TcpClient socket;
+    private NetworkStream stream;
+    private StreamWriter writer;
+    private StreamReader reader;
+
+    public void ConnectToServer()
+    {
+        if (socketReady)
+            return;
+
+
+        string host;
+        string name;
+        int port;
+
+        host = GameObject.Find("IPInput").GetComponent<InputField>().text;
+        name = GameObject.Find("NameInput").GetComponent<InputField>().text;
+        clientName = name;
+        int.TryParse(GameObject.Find("PortInput").GetComponent<InputField>().text, out port);
+
+        //create the socket
+
+        try
+        {
+            socket = new TcpClient(host, port);
+            stream = socket.GetStream();
+            writer = new StreamWriter(stream);
+            reader = new StreamReader(stream);
+            socketReady = true;
+            LoginPanel.SetActive(false);
+            TicTacToe.SetActive(true);
+        }
+        catch(Exception e)
+        {
+            LoginPanel.SetActive(true);
+            TicTacToe.SetActive(false);
+            Debug.Log("Socket error: " + e.Message);
+        }
+    }
+
+    public void OnClickSend()
+    {
+
+    }
+    private void Update()
+    {
+        if (Server.serverStarted)
+        {
+            HostButton.SetActive(false);
+        }
+
+        if (socketReady)
+        {
+            if (stream.DataAvailable)
+            {
+                string data = reader.ReadLine();
+                if (data != null)
+                    OnIncomingData(data);
+                //X and O
+            }
+        }
+
+        if(tempSendInfo != Mark.sendInfo)
+        {
+            tempSendInfo = Mark.sendInfo;
+            Send("&Mark|" + tempSendInfo);
+            
+        }
+    }
+
+    private void OnIncomingData(string data)
+    {
+        if(data == "%NAME")
+        {
+            Send("&NAME|" + clientName);
+            return;
+        }
+        GameObject go = Instantiate(MessagePrefab, ChatContainer.transform);
+        go.GetComponentInChildren<Text>().text = data;
+    }
+
+    private void Send(string data)
+    {
+        if (!socketReady)
+            return;
+        writer.WriteLine(data);
+        writer.Flush();
+    }
+
+    public void OnSendButton()
+    {
+        //button tic tac toe 
+        string message = GameObject.Find("SendInput").GetComponent<InputField>().text;
+        Send(message);
+    }
+
+    private void CloseSocket()
+    {
+        if (!socketReady)
+            return;
+
+        writer.Close();
+        reader.Close();
+        socket.Close();
+        socketReady = false;
+    }
+
+    private void OnApplicationQuit()
+    {
+        CloseSocket();
+    }
+    private void OnDisable()
+    {
+        CloseSocket();
+    }
+}
---
+++ Assets/Scripts/GameScripts/Mark.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Mark : MonoBehaviour {
+    public SpriteRenderer X;
+    public SpriteRenderer O;
+    public SpriteRenderer box;
+
+    public static string sendInfo;
+
+    public void OnMouseDown()
+    {
+        if(box.sprite == null)
+        {
+            if (GameManager.round % 2 == 0)
+            {
+                box.sprite = X.sprite;
+                Debug.Log("X");
+                GameManager.round++;
+            }
+            else
+            {
+                box.sprite = O.sprite;
+                Debug.Log("O");
+                GameManager.round++;
+            }
+
+            sendInfo = this.gameObject.name;
+        }
+        else
+        {
+            Debug.Log("this slot is already Taken");
+        }
+        
+        //else if(itsPlayer2)
+        //box.sprite = O.sprite;
+        //Debug.Log("O")
+    }
+}
